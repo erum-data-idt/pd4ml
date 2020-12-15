ee06df3d6dd9af9f2e66891972aa9289f7bdc642
buhmae <erik.buhmann@physik.uni-hamburg.de>
2020-12-15 15:16:49 +0100

    added file for pulling as package from git

---
+++ erum_data_data/__init__.py
@@ -0,0 +1,1 @@
+import erum_data_data
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='erum_data_data',
+      version='0.1',
+      description='setup for downloading data comparison data',
+      url='https://github.com/erikbuh/erum_data_data',
+      author='Erik Buhmann',
+      author_email='erik.buhmann@uni-hamburg.de',
+      license='MIT',
+      packages=['erum_data_data'],
+      zip_safe=False)
