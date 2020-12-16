dc2e4332e20c9e132dc5e83dadcf4ffb0d8d15c9
buhmae <erik.buhmann@physik.uni-hamburg.de>
2020-12-16 19:27:03 +0100

    updated descriptions, updated example skript, deleted test script

--- erum_data_data/erum_data_data.py
+++ erum_data_data/erum_data_data.py
@@ -7,37 +7,28 @@ import numpy as np
 
 # dictionary of urls from DESY cloud, filenames and md5 checksums
 URL_DICT = {'test_filenames':[
-                '1_top_tagging_1_2M.npz',
+                '1_top_tagging_2M.npz',
                 '2_spinodal_200k.npz',
                 '3_EOSL_or_EOSQ_180k.npz',
                 '4_airshower_100k.npz',
-                '5_LHCOlympics2020_350k.npz',
+                '5_LHCOlympics2020_features_1_1M.npz',
                 '6_belle-selective_400k.npz',
-                'test.npy',
-                'test_half1.npy',
-                'test_half2.npy',
                 ],
              'test_urls': [
-                 'https://desycloud.desy.de/index.php/s/ZcCtQ6G8df4F5px/download',
-                 'https://desycloud.desy.de/index.php/s/gfd79exSBgYqspA/download',
-                 'https://desycloud.desy.de/index.php/s/rLrLPLZDTtBLd5n/download',
+                 'https://desycloud.desy.de/index.php/s/xXdGE7saNNCAZCK/download',
+                 'https://desycloud.desy.de/index.php/s/CrKz3cy3XcMpxpJ/download',
+                 'https://desycloud.desy.de/index.php/s/DT7sWm6rNR5zss9/download',
                 'https://desycloud.desy.de/index.php/s/Qcn38RExyDstEAP/download',
-                 'https://desycloud.desy.de/index.php/s/izD3CN8nw68nWPp/download',
+                 'https://desycloud.desy.de/index.php/s/4DaZCFgmZ83o9FT/download',
                  'https://desycloud.desy.de/index.php/s/Yd885LGfdMqoxbq/download',
-                'https://desycloud.desy.de/index.php/s/aCcYSZmzHi3RXKm/download',
-                'https://desycloud.desy.de/index.php/s/LJkzgbg6C4JRwTY/download',
-                'https://desycloud.desy.de/index.php/s/mctQMTo7gfNmAa2/download',
                  ],
             'test_md5': [
-                '7cee4581389e269d9214d51bcaa58fce',
-                '7808e5dd8d0fa7e6a7f26991fd69a23f',
-                '66a12b7ac5f2e2b5db5bffb216654ef5',
+                '3d27b9ddd0ba4bbebe10bf2a1f9bcc3c',
+                '89f0ea8c9e01a13c549d2acb60be2839',
+                'be5563e787f856bd6b2f9fe171277bc3',
                 'fcc0784f2dd781d7b1e02754fbffb360',
-                'e996ca33ce3fa89877b7cb6bfad449ea',
+                'd9a29ff15679c7034b7acf58e82afa0f',
                 '5315e853616d12c39b590ff6d3fcc95e',
-                '3ecb1f1e4f2c624d4e4361877c7466e2',
-                'f96b949fc84dbce69f4e47f2eb66690a',
-                '55fe2c80f22b92247eb85805d6f17f1e',
                 ],
              }
 
@@ -196,15 +187,50 @@ All six datasets are tagged with:
 ''')
     
     top_description = ('''
-lets write
+Top tagging dataset.
+
+Description:
+14tev, hadronic tops for signal, qcd diets background, delphes ATLAS detector card with pythia. No MPI/pile-up included
+Particle-flow entries (produced by Delphes E-flow) have been clustered into anti-kT 0.8 jets in the pT range [550,650].
+All top jets are matched to a parton-level top within ∆R = 0.8, and to all top decay partons within 0.8. Also,|eta|_jet < 2 has been required.
+
+Ref: 
+Deep-learned Top Tagging with a Lorentz Layer by A Butter, G Kasieczka, T and M Russell (arXiv: 1707.08966)
+
+Dataset shape:
+~2M events have been stored divided between training (70%) and test (30%) and the shape of the dataset is (# of events, 201, 4).
+The feature represent the leading 200 jet constituent four-momenta, with zero-padding for jets that have less than 200.
+Constituents are sorted by pT, with the highest pT one first.
+The truth top four-momentum is stored as truth_px, truth_py, etc.
+The set label are 0 for QCD and 1 for top.
 ''')
     
     spinodal_description = ('''
-a bit of text 
+Spinodal dataset.
+Description:
+The goal of this classification is to identify the nature of the QCD phase transitic collisions at the CBM experiment and, in particular, whether signals for b-associated with the phase transition can be ound in the final momentum spectra of certain collisions.
+
+Ref:
+J. Steinheimer, L. Pang, K. Shou, V. Koch, J. Randrup and H.Stoecker, JHEP 19 doi:10.1007/JHEP12(2019)122 [arXiv:1906.06562 [nucl-th]]
+
+Dataset shape:
+The dataset is composed of 200'000 2Dhistograms of shape 20x20 describing pion momenta, divided in training (70%) and test (30%). So, the shape of the dataset is (# of events, 20, 20).
+The set label is 1 for a Spinodal event and 0 for a Maxwell event.
 ''')
     
     EOSL_description = ('''
-another text
+EOSL or EOSQ dataset.
+
+Description:
+The task here is to classify the QCD transition nature (two different equation of state: cross-over EOSL or 1st order EOSQ) happened in heavy-ion collisions from the final state pion spectra. The pion spectra in transverse momentum and azimuthal angle are simulated here with Hybrid (Viscous Hydrodynamics plus hadronic cascade UrQMD) modeling for heavy-ion collisions by varing different physical  arameters (collision energy, entrality, initial time, initial ondition models with fluctuations, shear viscousity, freeze-out temperature, switch time from hydro to hadronic cascade)
+
+Ref:
+An equation-of-state-meter of quantum chromodynamics transition from deep learning, Long-Gang Pang, Kai Zhou, Nan Su, Hannah Petersen, Horst Stoecker and Xin-Nian Wang, Nature Commun.9 (2018) no.1,210
+Identifying the nature of the QCD transition in relativistic collision of heavy nuclei with deep learning, Yi-Lun Du, Kai Zhou, Jan Steinheimer, Long-Gang Pang, Anton Motornenko, Hong-Shi Zong, Xin-Nian Wang and Horst Stoecker, arXiv:1910.11530
+
+Dataset shape:
+The dataset is composed of ~180'000 2Dhistogram of shape 24x24 of the pion spectra and it is divided in 70% training and 30% test. So, the shape of the dataset is (# of events, 24, 24).
+Label is 1 for EOSQ and 0 for EOSL.
 ''')
     
     airshower_description = ('''
@@ -251,17 +277,26 @@ Proton to Iron ratio in test & training set is 1:1.
     
 R&D Dataset for LHC Olympics 2020 Anomaly Detection Challenge
 
-Classification between QCD dijet events and W'->XY events.
+Classification between 1M QCD dijet events and 100k W'->XY events. Hence this dataset is unbalanced with a label 0 to 1 ratio of 10:1. 
 
 More information from the authors: https://zenodo.org/record/4287694#.X9o1_C1Q1pR
+The dataset 'events_anomalydetection_v2.features.h5' was used and split into two a training set (70%) and a test set (30%).
 
 ----------------------------------    
 Dataset shape:
+- one numpy array of the shape (no. of events, 15)
+    - 15 features, namely:
 
+        The features (calculated using fastjet plugins) are the 3-momenta, invariant masses, 
+        and n-jettiness variables tau1, tau2 and tau3 for the highest pT jet (j1) 
+        and the second highest pT jet (j2):
+    
+        'pxj1', 'pyj1', 'pzj1', 'mj1', 'tau1j1', 'tau2j1', 'tau3j1', 
+        'pxj2', 'pyj2', 'pzj2', 'mj2', 'tau1j2', 'tau2j2', 'tau3j2'
 
 ---------------------------------- 
 Label: 
-
+0: QCD dijet event, 1: W'-> XY event (ratio: 10:1)
 ''')
     
     belle_description = ('''
