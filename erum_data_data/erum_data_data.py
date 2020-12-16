ee3b947c007c723e9a051b134edb25a66f1145d9
buhmae <erik.buhmann@physik.uni-hamburg.de>
2020-12-16 17:40:24 +0100

    added test, train set separation, added print description function

--- erum_data_data/erum_data_data.py
+++ erum_data_data/erum_data_data.py
@@ -106,11 +106,32 @@ def _get_filepath(filename, url, md5, cache_dir = './', cache_subdir = 'datasets
 
 
 
-def load(dataname, cache_dir = './', cache_subdir = 'datasets'):
+def load(dataname, dataset = 'train', cache_dir = './', cache_subdir = 'datasets'):
     ''' 
     loads a datafile from a list of options 
     Returns a list of X feature numpy arrays for test and training set
     as well as numpy arrays for test and training label
+    
+    Additional descriptions of the datasets can be printed via: 
+    
+    erum_data_data.print_description() function
+    
+    Parameters
+    ----------
+    dataname: Name of dataset from a list of options:  
+        1: 'top', 2: 'spinodal', 3: 'EOSL', 4: 'airshower', 5: 'LHCO', 6: 'belle'
+    datset: chosse the training or testing set:
+        dataset = 'train' or 'test'
+    cache_dir: directory where the datasets are saved
+        cache_dir = './'
+    cache_subdir: name of subdirectory
+        cache_subdir = 'datasets'
+        
+    Returns
+    -------
+    X, y
+    X: a list of numpy arrays with X input features - see print_decription() for more details
+    y: a numpy array with labels [0,1]
     '''
 
     if dataname == 'top':
@@ -137,11 +158,150 @@ def load(dataname, cache_dir = './', cache_subdir = 'datasets'):
     
     np_zip = np.load(fpath)
     
-    X_train, X_test = [], []
+    X = []
     for i in range(int(len(np_zip)/2 - 1)):
-        X_train.append(np_zip['X_train_{}'.format(i)])
-        X_test.append(np_zip['X_test_{}'.format(i)])
-    y_train = np_zip['y_train']
-    y_test = np_zip['y_test']
+        X.append(np_zip['X_{}_{}'.format(dataset, i)])
+    y = np_zip['y_{}'.format(dataset)]
     
-    return X_train, y_train, X_test, y_test
+    return X, y
+
+
+def print_description(dataname=None):
+    '''
+    This is a helper function that prints a description of the different datasets.
+
+    Parameters for the datasets are, i.e. dataname='top'.
+
+    All six datasets are tagged with:
+    1: 'top'
+    2: 'spinodal'
+    3: 'EOSL'
+    4: 'airshower'
+    5: 'LHCO'
+    6: 'belle'
+    '''
+
+    none_description = ('''
+This is a helper function that prints a description of the different datasets.
+
+Parameters for the datasets are, i.e. dataname='top'.
+
+All six datasets are tagged with:
+1: 'top'
+2: 'spinodal'
+3: 'EOSL'
+4: 'airshower'
+5: 'LHCO'
+6: 'belle'
+''')
+    
+    top_description = ('''
+lets write
+''')
+    
+    spinodal_description = ('''
+a bit of text 
+''')
+    
+    EOSL_description = ('''
+another text
+''')
+    
+    airshower_description = ('''
+    
+Airshower Proton vs Iron Classification
+
+Based on https://doi.org/10.1016/j.astropartphys.2017.10.006
+
+Produced by jonas.glombitza@rwth-aachen.de
+    
+----------------------------------    
+Dataset shape:
+
+Three sets of input data:
+- first set of input data:
+    - 70k events (airshowers)
+    - 81 ground detector stations
+    - 81 features
+        - 1  time   (arrival time of first particles at each station)
+        - 80 measured signal traces
+    -padding: (-1) padding for instances that the detector / or timestep did not detect a particle
+    
+- second set of input data:
+    - 70k events (airshowers)
+    - 11 features per airshower:
+        'logE',  --> (energy of cosmic ray)
+        'Xmax',  --> (depth of shower maximum)  
+        'showermax_x', 'showermax_y', 'showermax_z',     --> (point of showermaximum in x,y,z)
+        'showeraxis_x', 'showeraxis_y', 'showeraxis_z',  --> (shower axis (arrival direction) in x,y,z)
+        'showercore_x', 'showercore_y', 'showercore_z',  --> (shower core (intersection shower axis with detector plane) in x,y,z)
+    
+- thrid set of input data
+    - detector geometry - for reference if needed
+    - 81 ground detector stations
+    - 3 features: x,y,z location of each station
+  
+---------------------------------- 
+Label: 
+Proton (1) vs. Iron (0) as shower origin particle.
+Proton to Iron ratio in test & training set is 1:1.
+''')
+    
+    LHCO_description = ('''
+    
+R&D Dataset for LHC Olympics 2020 Anomaly Detection Challenge
+
+Classification between QCD dijet events and W'->XY events.
+
+More information from the authors: https://zenodo.org/record/4287694#.X9o1_C1Q1pR
+
+----------------------------------    
+Dataset shape:
+
+
+---------------------------------- 
+Label: 
+
+''')
+    
+    belle_description = ('''
+
+SmartBKG dataset (Belle II - generated events passing downstream selection)
+
+The goal of this classification problem is to identify generated events that pass a selection already before the expensive detector simulation and reconstruction.
+
+Original dataset and additional information: https://github.com/kahn-jms/belle-selective-mc-dataset
+
+----------------------------------    
+Dataset shape:
+
+Two sets of input data: 
+- first set with shape:
+    - 280000 belle collider events
+    - 100 particles (zero padded)
+    - 9 features ('prodTime', 'energy', 'x', 'y', 'z', 'px', 'py', 'pz', 'PID')
+        - note: PID corresponding to a unique PDG particle ID, but mapped to a continous space
+        
+- second set with shape:
+    - 280000 belle collider events
+    - 100 indices of mother particles (adjacency matrix for creating a graph of the event)
+        - note: these are -1 padded
+---------------------------------- 
+Label: 
+event passes (1) or fails (0) a selection that was applied after detector simulation and reconstruction
+''')
+   
+    if dataname == 'top':
+        return print(top_description)
+    elif dataname == 'spinodal':
+        return print(spinodal_description)
+    elif dataname == 'EOSL':
+        return print(EOSL_description)
+    elif dataname == 'airshower':
+        return print(airshower_description)
+    elif dataname == 'LHCO':
+        return print(LHCO_description)
+    elif dataname == 'belle':
+        return print(belle_description)
+    else:
+        return print(none_description)
