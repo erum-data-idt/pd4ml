8f62d5c439eed374304f49cf9f781851434b2c10
WilliamKorcari <william.korcari@desy.de>
2021-02-02 14:54:06 +0100

    added for-loop over all compatible datasets

--- template_model_implamentation/main.py
+++ template_model_implamentation/main.py
@@ -7,12 +7,12 @@ import tensorflow as tf
 ##	utils.py is the file that contains all the self-built methods of this script.
 ##	Please add the model generating function and the preprocessing function in that file.
 from fcn import Fcn		    	#import your model function
-'''
+
 from utils import train_plots
 from utils import roc_auc
 from utils import test_accuracy
 from utils import test_f1_score
-'''
+
 
 
 #########################################
@@ -24,30 +24,29 @@ nn = Fcn()
 
 datasets =  nn.compatible_datasets
 
-ds = datasets[1] #spinodal dataset 
+for ds in datasets:  
 
-X_train, y_train  = edd.load(ds, dataset='train', cache_dir = './'+ ds, cache_subdir = 'datasets')
-X_test, y_test = edd.load(ds, dataset='test', cache_dir = './'+ ds, cache_subdir = 'datasets')
+	X_train, y_train  = edd.load(ds, dataset='train', cache_dir = './'+ ds, cache_subdir = 'datasets')
+	X_test, y_test = edd.load(ds, dataset='test', cache_dir = './'+ ds, cache_subdir = 'datasets')
 
-x_train = nn.preprocessing(X_train[0])
-x_test = nn.preprocessing(X_test[0])
+	x_train = nn.preprocessing(X_train[0])
+	x_test = nn.preprocessing(X_test[0])
 
 
-model = nn.model(ds, shape = x_train.shape[1:])
-model.compile(**nn.compile_args)
-history = model.fit(x = x_train, y = y_train, **nn.fit_args)
+	model = nn.model(ds, shape = x_train.shape[1:])
+	model.compile(**nn.compile_args)
+	history = model.fit(x = x_train, y = y_train, **nn.fit_args)
 
-##	From here on, one should be able to use already defined methods as showed in the following lines. 
-##	Let us know if you face any issues with that.
-'''
-#training history plots
-train_plots(history, ds, True)
+	##	From here on, one should be able to use already defined methods as showed in the following lines. 
+	##	Let us know if you face any issues with that.
 
-#evaluation plots and scores
-y_pred = model.predict(X_test).ravel()
-roc_auc(y_pred, y_test, ds, True)
-test_accuracy(y_pred, y_test, ds)
-test_f1_score(y_pred, y_test, ds)
+	#training history plots
+	train_plots(history, ds, True)
 
-model.load_weights(checkpoint_path)
-'''
+	#evaluation plots and scores
+	y_pred = model.predict(X_test).ravel()
+	roc_auc(y_pred, y_test, ds, True)
+	test_accuracy(y_pred, y_test, ds)
+	test_f1_score(y_pred, y_test, ds)
+
+
