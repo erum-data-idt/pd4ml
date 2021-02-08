e47f7ef6da908454b2d7c151015b935e75174623
WilliamKorcari <william.korcari@desy.de>
2021-02-08 10:53:47 +0100

    template_model_implamentation/fcn.py
    fixed preprocessing to match main.py implementation

--- template_model_implamentation/main.py
+++ template_model_implamentation/main.py
@@ -2,8 +2,8 @@
 ##	William Korcari: william.korcari@desy.de
 
 ##	Please add the model generating function and the preprocessing function in that file.
-# from fcn import Network		    	#import your model function
-from gcn_belle import Network
+from fcn import Network		    	#import your model function
+#from gcn_belle import Network
 
 ##	utils.py is the file that contains all the self-built methods of this script.
 from utils import train_plots
