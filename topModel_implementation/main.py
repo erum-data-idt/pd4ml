3e15f1854d550361172f2bc304383c6f526a27f9
Lisa Benato <lisa.benato@cern.ch>
2021-02-10 19:59:24 +0100

    Preprocessing returning an output in the same format as in the other models

--- topModel_implementation/main.py
+++ topModel_implementation/main.py
@@ -27,12 +27,16 @@ for ds in datasets:
 
 	#max_e = 100000#<-- this trains already well#1000#-1#100000
 	#max_t = 10000#-1#100000
-	x_train = nn.preprocessing(X_train[0])#(X_train[0][0:max_e])
-	x_test  = nn.preprocessing(X_test[0])#(X_test[0][0:max_t])
+	#X_train[0] = X_train[0][0:max_e]
+	#X_test[0] = X_test[0][0:max_t]
+	#y_train = y_train[0:max_e]
+	#y_test = y_test[0:max_t]
+	x_train = nn.preprocessing(X_train[0])
+	x_test  = nn.preprocessing(X_test[0])
 	
 	model = nn.model_lite(nn.get_shapes(x_train))
 	model.compile(**nn.compile_args)
-	history = model.fit(x = x_train.X, y = y_train, **nn.fit_args)
+	history = model.fit(x = x_train, y = y_train, **nn.fit_args)
 
 	##	From here on, one should be able to use already defined methods as showed in the following lines. 
 	##	Let us know if you face any issues with that.
@@ -42,7 +46,7 @@ for ds in datasets:
 	train_plots(history, label, True)
 
 	#evaluation plots and scores
-	y_pred = model.predict(x_test.X).ravel()
+	y_pred = model.predict(x_test).ravel()
 	roc_auc(y_pred, y_test, label, True)
 	test_accuracy(y_pred, y_test, label)
 	test_f1_score(y_pred, y_test, label)
