266a71b8ef271684a3179ede0dcb18c46c125753
Lisa Benato <lisa.benato@cern.ch>
2021-02-22 14:12:38 +0100

    Rearranged order of plot/write output  score in .txt

--- models/main.py
+++ models/main.py
@@ -3,8 +3,10 @@
 
 ## import of the models
 #from fcn import Network		    	#import your model function
+from simple_graph_net import Network
+#from simple_graph_net_belle2 import Network
 #from gcn_belle import Network
-from cnn_spinodal import Network
+#from cnn_spinodal import Network
 #from particle_net import Network
 ##	utils.py is the file that contains all the self-built methods of this script.
 from utils import train_plots
@@ -26,8 +28,8 @@ for ds in datasets:
     X_test, y_test = ds.load(split="test")
     x_train = nn.preprocessing(X_train)
     x_test = nn.preprocessing(X_test)
-    
-     
+
+
     model = nn.model(ds, shapes=nn.get_shapes(x_train))
     model.compile(**nn.compile_args)
     history = model.fit(x=x_train, y=y_train, **nn.fit_args)
@@ -35,11 +37,11 @@ for ds in datasets:
     ##	From here on, one should be able to use already defined methods as showed in the following lines.
     ##	Let us know if you face any issues with that.
 
-    # training history plots
-    train_plots(history, ds.name, True)
-
     # evaluation plots and scores
     y_pred = model.predict(x_test).ravel()
-    roc_auc(y_pred, y_test, ds.name, True)
     test_accuracy(y_pred, y_test, ds.name, nn.model_name)
     test_f1_score(y_pred, y_test, ds.name, nn.model_name)
+
+    # training history plots
+    train_plots(history, ds.name, True)
+    roc_auc(y_pred, y_test, ds.name, True)
