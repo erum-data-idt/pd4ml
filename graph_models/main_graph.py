bfa8669d850d7b23d267f5c72682c5b913fcc776
Nikolai Hartmann <nikolai.hartmann@physik.uni-muenchen.de>
2021-03-09 12:08:32 +0100

    revert graph_models/main_graph.py

--- graph_models/main_graph.py
+++ graph_models/main_graph.py
@@ -10,8 +10,6 @@ from utils import test_accuracy
 from utils import test_f1_score
 
 from os import chdir
-import numpy as np
-import tensorflow as tf
 #########################################
 #####  EXAMPLE IMPLEMENTATION OF FCN  ###
 
@@ -19,50 +17,24 @@ nn = Network()
 
 datasets = nn.compatible_datasets
 
-# set to true to cache tf data datasets
-# (if enough memory is available)
-cache_tf_data = True
-
 for ds in datasets:
 
-    if not hasattr(ds, "load_graph_tf_data"):
-        x_train, y_train = ds.load_graph('train', path = './datasets')
-        x_test, y_test = ds.load_graph('test', path = './datasets')
-        model = nn.model(ds, shapes=nn.get_shapes(x_train))
-        model.compile(**nn.compile_args)
-        print(model.summary())
-        history = model.fit(x=x_train, y=y_train, **nn.fit_args)
-        y_pred = model.predict(x_test).ravel()
-    else:
-        fit_args = dict(nn.fit_args)
-        batch_args = dict(
-            validation_split=fit_args.pop("validation_split"),
-            batch_size=fit_args.pop("batch_size")
-        )
-        tf_ds_train, tf_ds_val = ds.load_graph_tf_data('train', path = './datasets', **batch_args)
-        tf_ds_test = ds.load_graph_tf_data('test', path = './datasets', **dict(batch_args, validation_split=None))
-        example_batch = next(iter(tf_ds_train))
-        model = nn.model(ds, shapes=nn.get_shapes(example_batch[0]))
-        model.compile(**nn.compile_args)
-        print(model.summary())
-        if not cache_tf_data:
-            history = model.fit(tf_ds_train, validation_data=tf_ds_val, **fit_args)
-        else:
-            history = model.fit(
-                tf_ds_train.cache().repeat(),
-                validation_data=tf_ds_val.cache().repeat(),
-                steps_per_epoch=int(tf.data.experimental.cardinality(tf_ds_train)),
-                validation_steps=int(tf.data.experimental.cardinality(tf_ds_val)),
-                **fit_args
-            )
-        y_pred = model.predict(tf_ds_test.map(lambda x, y: x)).ravel()
-        y_test = np.concatenate([batch.numpy() for batch in tf_ds_test.map(lambda x, y: y)])
+    x_train, y_train = ds.load_graph('train', path = './datasets')
+
+    x_test, y_test = ds.load_graph('test', path = './datasets')
+    
+    model = nn.model(ds, shapes=nn.get_shapes(x_train))
+    model.compile(**nn.compile_args)
+    print(model.summary())
+    history = model.fit(x=x_train, y=y_train, **nn.fit_args)
 
     ##	From here on, one should be able to use already defined methods as showed in the following lines.
     ##	Let us know if you face any issues with that.
 
+    # training history plots
     label = '_test_graph'
-
+    # evaluation plots and scores
+    y_pred = model.predict(x_test).ravel()
     test_accuracy(y_pred, y_test, ds.name+label, nn.model_name)
     test_f1_score(y_pred, y_test, ds.name+label, nn.model_name)
 
